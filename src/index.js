5ae2d900f3df603b66e2c600260df295afa1f7db
JoshDT900 <DJTroutman900@gmail.com>
2022-11-02 11:13:42 -0400

    setup initial dom module for creating elements

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-import _ from 'lodash';
 import './style.css';
+
+const domGenModule = (function() {
+
+    const makeEle = function() {
+        let argArr = [...arguments]
+
+        if (typeof(argArr[0]) != 'string') {
+            return alert("First argument must be a valid HTML element in string format. Example: makeEle('div') ")
+        }
+
+        let newEle = document.createElement(`${argArr[0]}`)        
+
+        if (argArr.length >= 2 && argArr[1].length === 2) {
+            newEle.setAttribute(...argArr[1]);
+            
+            if (argArr.length === 3) {
+                if (typeof(argArr[2]) === 'string') {
+                    newEle.textContent = argArr[2];
+                } else {
+                    return alert("The third argument must be a string")
+                }
+            }
+        } else {
+            return alert("The second argument must be an array of 2 strings.")
+        }
+
+        console.log(newEle);
+        return newEle;
+    }
+
+    return { makeEle }
+})();
+
+domGenModule.makeEle("div", ["class", "test"], "3")
 
