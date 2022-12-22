47f7673b30ac64613f600f9e7917a0aa97f3ec2d
JoshDT900 <DJTroutman900@gmail.com>
2022-12-22 16:14:11 -0500

    Fix some issues

--- src/datafile.js
+++ src/datafile.js
@@ -1,7 +1,7 @@
 let dataObjArr = []
 
 let loadData = (locStor) => {
-    if (locStor.projects === undefined) {
+    if (!locStor.projects) {
         return dataObjArr = [];
     } else {
         return dataObjArr = JSON.parse(locStor.projects);
